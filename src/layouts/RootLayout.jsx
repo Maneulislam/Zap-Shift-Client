353b0714693d4a119c9f748b787459d85c06ccf4
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2026-08-10 12:40:50 +0600

    Setup and add Navbar

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router';
+import NavBar from '../pages/Shared/NavBar/NavBar';
+import Footer from '../pages/Shared/Footer/Footer';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default RootLayout;
