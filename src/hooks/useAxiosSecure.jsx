37416c60d4172aa249da3fc5228674590c7138cf
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2026-08-30 19:34:03 +0600

    verifyToken Middleware and logout unauthorized access

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 
 
@@ -7,6 +10,46 @@ const instanceAxios = axios.create({
 })
 
 const useAxiosSecure = () => {
+
+    const navigate = useNavigate();
+
+    const { user, logOut } = useAuth();
+    console.log(user);
+
+    useEffect(() => {
+        const reqInterceptor = instanceAxios.interceptors.request.use(config => {
+
+            config.headers.Authorization = `Bearer ${user?.accessToken} `
+
+            return config;
+        })
+
+
+        const resInterceptor = instanceAxios.interceptors.response.use((response) => {
+            return response;
+        },
+            (error) => {
+                console.log(error);
+
+                const statusCode = error.status;
+                if (statusCode === 401 || statusCode === 403) {
+                    logOut();
+
+                    navigate('/login')
+
+                }
+
+
+                return Promise.reject(error);
+            })
+
+
+        return () => {
+            instanceAxios.interceptors.request.eject(reqInterceptor);
+            instanceAxios.interceptors.response.eject(resInterceptor)
+        }
+
+    }, [user])
 
 
     return instanceAxios;
